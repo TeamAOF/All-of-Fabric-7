0954bf43fabfd9420f69f22b91aded8409fe29d6
AK9 <anthonkrisat@hotmail.com>
2023-10-20 03:49:02 +0200

    fixed certus quartz recipe in industrial grinder

---
+++ kubejs/server_scripts/recipetypes/techreborn/industrial_grinder.js
@@ -0,0 +1,27 @@
+////////////////////////
+/// Made by Team AOF ///
+////////////////////////
+
+
+ServerEvents.recipes(event => {
+
+event.custom({    
+  "type": "techreborn:industrial_grinder",
+  "power": 128,
+  "time": 100,
+  "tank": {
+    "fluid": "minecraft:water",
+    "amount": 1000
+  },
+  "ingredients": [
+	{
+	  "tag": "c:certus_quartz"
+	}
+  ],
+  "results": [
+	{
+	  "item": "ae2:certus_quartz_dust"
+	}
+  ]
+})
+    })
