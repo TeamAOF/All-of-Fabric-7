a16ae0108522144d73def2c18f07c9ac48ae5e2e
Kazuhiko-Gushiken <132615999+Kazuhiko-Gushiken@users.noreply.github.com>
2023-10-21 22:21:29 -0700

    Fixed my accidental code replacement
    sorry AK

--- kubejs/server_scripts/recipetypes/packedup/shaped.js
+++ kubejs/server_scripts/recipetypes/packedup/shaped.js
@@ -1,42 +1,113 @@
+
 ////////////////////////
 /// Made by Team AOF ///
 ////////////////////////
 
+
 ServerEvents.recipes(event => {
 
-    const recipes = [
-      {
-        output: 'packedup:silverbackpack',
-        pattern: ['SSS', 'SIS', 'SSS'],
-        key: {
-          S: 'modern_industrialization:silver_ingot',
-          I: 'packedup:ironbackpack',
+  const recipes = [
+
+    // Silver from Basic
+    {
+      output: 'packedup:silverbackpack',
+      pattern: [
+        "ABA",
+        "ACA",
+        "AAA"
+      ],
+      key: {
+        A: '#c:silver_ingots',
+        B: 'minecraft:leather',
+        C: 'packedup:basicbackpack',
       },
-        id: 'aof:silver_backpack_from_iron'
+      id: 'packedup:silver_backpack_from_basic'
+    },
+
+    // Silver from Iron
+    {
+      output: 'packedup:silverbackpack',
+      pattern: [
+        "ABA",
+        "ACA",
+        "AAA"
+      ],
+      key: {
+        A: '#c:silver_ingots',
+        B: '#c:iron_ingots',
+        C: 'packedup:ironbackpack',
       },
-      {
-        output: 'packedup:silverbackpack',
-        pattern: ['SSS', 'SCS', 'SSS'],
-        key: {
-            S: 'modern_industrialization:silver_ingot',
-            C: 'packedup:copperbackpack',
-        },
-          id: 'aof:silver_backpack_from_copper'
+      id: 'packedup:silver_backpack_from_iron'
+    },
+
+    // Silver from Copper
+    {
+      output: 'packedup:silverbackpack',
+      pattern: [
+        "ABA",
+        "ACA",
+        "AAA"
+      ],
+      key: {
+        A: '#c:silver_ingots',
+        B: '#c:copper_ingots',
+        C: 'packedup:copperbackpack',
       },
-      {
-        output: 'packedup:diamondbackpack',
-        pattern: ['DGD', 'DSD', 'GGG'],
-        key: {
-            D: 'minecraft:diamond',
-            G: 'minecraft:gold_ingot',
-            S: 'packedup:silverbackpack',
-            G: '#c:glass_blocks',
-        },
-          id: 'aof:diamond_backpack_from_silver'
-      }
-    ];
-  
-    recipes.forEach((recipe) => {
-      event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.id);
-    });
+      id: 'packedup:silver_backpack_from_copper'
+    },
+
+    // Silver from Chest
+    {
+      output: 'packedup:silverbackpack',
+      pattern: [
+        "ABA",
+        "CDC",
+        "CCC"
+      ],
+      key: {
+        A: 'minecraft:leather',
+        B: 'minecraft:string',
+        C: '#c:silver_ingots',
+        D: 'minecraft:chest',
+      },
+      id: 'packedup:silver_backpack_from_chest'
+    },
+
+    // Diamond from Silver
+    {
+      output: 'packedup:diamondbackpack',
+      pattern: [
+        "ABA",
+        "ACA",
+        "DDD"
+      ],
+      key: {
+        A: '#c:diamonds',
+        B: '#c:silver_ingots',
+        C: 'packedup:silverbackpack',
+        D: '#c:glass_blocks',
+      },
+      id: 'packedup:diamond_backpack_from_silver'
+    },
+
+    // Obsidian from Silver
+    {
+      output: 'packedup:obsidianbackpack',
+      pattern: [
+        "ABA",
+        "ACA",
+        "AAA"
+      ],
+      key: {
+        A: 'minecraft:obsidian',
+        B: '#c:silver_ingots',
+        C: 'packedup:silverbackpack',
+      },
+      id: 'packedup:obsidian_backpack_from_silver'
+    },
+  ];
+
+  recipes.forEach((recipe) => {
+    event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.id);
   });
+});
