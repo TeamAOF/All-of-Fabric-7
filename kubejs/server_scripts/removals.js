8fba0afc166c90a1f0dc8b178d0bf0bc57637987
AK9 <anthonkrisat@hotmail.com>
2023-12-11 23:40:05 +0100

    remove duplicate UU matter recipe

--- kubejs/server_scripts/removals.js
+++ kubejs/server_scripts/removals.js
@@ -81,7 +81,8 @@ ServerEvents.recipes((event) => {
         'modern_industrialization:quarry/gold',
         'modern_industrialization:quarry/steel',
         'modern_industrialization:quarry/titanium',
-        'techreborn:industrial_grinder/certus_quartz_ore'
+        'techreborn:industrial_grinder/certus_quartz_ore',
+        'modern_industrialization:materials/electrolyzer/uu_matter',
 
     ];
 
