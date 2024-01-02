381c299874c9fc20191d376773aeceef97c6a6fc
Kazuhiko-Gushiken <132615999+Kazuhiko-Gushiken@users.noreply.github.com>
2024-01-02 11:55:55 -0800

    removed dud recipe

--- kubejs/server_scripts/recipetypes/create/pressing.js
+++ kubejs/server_scripts/recipetypes/create/pressing.js
@@ -2229,30 +2229,4 @@ event.custom({
     ],
     heatRequirement: 'heated'
 }).id('z_aof:compacting/bloodstone')
-
-event.custom({
-    type: "create:compacting",
-    ingredients: [
-        {
-            fluid: "minecraft:water",
-            amount: 40500
-        },
-        {
-            fluid: "minecraft:lava",
-            amount: 40500
-        },
-        {
-            item: "kubejs:ultra_compressed_mixed_stone_ball"
-        }
-    ],
-    results: [
-        {
-            item: ""
-        },
-        {
-            item: "kubejs:ultra_compressed_mixed_stone_ball"
-        }
-    ],
-    heatRequirement: 'heated'
-}).id('z_aof:compacting/')
 })
