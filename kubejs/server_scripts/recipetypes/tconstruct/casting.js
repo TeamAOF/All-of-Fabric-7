734bc080457483fe2ef2003845ac2c9544de37cb
AK9 <anthonkrisat@hotmail.com>
2024-03-14 23:00:41 +0100

    custom tconstruct recipes for MI

---
+++ kubejs/server_scripts/recipetypes/tconstruct/casting.js
@@ -0,0 +1,97 @@
+ServerEvents.recipes(event => {
+
+  const aof_materials = [
+    'antimony',
+    'iridium',
+  ];
+
+  // Blocks
+  aof_materials.forEach((item) => {
+    event.custom({
+      "type": "tconstruct:casting_basin",
+      "fluid": {
+        "name": "kubejs:molten_" + item,
+        "amount": 81000
+      },
+      "result": "modern_industrialization:" + item + "_block",
+      "cooling_time": 225
+    })
+
+    // Ingots
+    event.custom({
+      "type": "tconstruct:casting_table",
+      "cast": {
+        "item": "tconstruct:ingot_cast"
+      },
+      "fluid": {
+        "name": "kubejs:molten_" + item,
+        "amount": 9000
+      },
+      "result": "modern_industrialization:" + item + "_ingot",
+      "cooling_time": 45
+    })
+  })
+
+  // Large Plates
+  const large_plates = [
+    'tin',
+    'lead',
+    'nickel',
+    'tungsten',
+    'steel',
+    'gold',
+    'invar',
+    'aluminum',
+    'bronze',
+    'copper',
+    'iron',
+    'diamond'
+  ];
+
+  large_plates.forEach((item) => {
+    event.custom({
+      "type": "tconstruct:casting_table",
+      "cast": {
+        "item": "tconstruct:large_plate_cast"
+      },
+      "fluid": {
+        "name": "tconstruct:molten_" + item,
+        "amount": 36000
+      },
+      "result": "modern_industrialization:" + item + "_large_plate",
+      "cooling_time": 90
+    })
+  })
+
+  // Plates
+  const aof_fluids = [
+    'iridium',
+  ];
+
+  aof_fluids.forEach((item) => {
+    event.custom({
+      "type": "tconstruct:casting_table",
+      "cast": {
+        "item": "tconstruct:plate_cast"
+      },
+      "fluid": {
+        "name": "kubejs:molten_" + item,
+        "amount": 9000
+      },
+      "result": "modern_industrialization:" + item + "_plate",
+      "cooling_time": 45
+    })
+    event.custom({
+      "type": "tconstruct:casting_table",
+      "cast": {
+        "item": "tconstruct:large_plate_cast"
+      },
+      "fluid": {
+        "name": "kubejs:molten_" + item,
+        "amount": 36000
+      },
+      "result": "modern_industrialization:" + item + "_large_plate",
+      "cooling_time": 90
+    })
+  })
+})
---
+++ kubejs/startup_scripts/fluids.js
@@ -0,0 +1,28 @@
+StartupEvents.registry('fluid', event => {
+
+  // Mozanite
+  event.create('molten_mozanite')
+    .thickTexture(0x8D2185)
+    .bucketColor(0x8D2185)
+    .displayName('Molten Mozanite')
+    .luminosity(8)
+    .noBucket()
+
+  // Antimony
+  event.create('molten_antimony')
+    .thickTexture(0xA5A5B4)
+    .bucketColor(0xA5A5B4)
+    .displayName('Molten Antimony')
+    .luminosity(8)
+    .noBucket()
+
+  // Iridium
+  event.create('molten_iridium')
+    .thickTexture(0xE5E5E5)
+    .bucketColor(0xE5E5E5)
+    .displayName('Molten Iridium')
+    .luminosity(8)
+    .noBucket()
+
+  // Alloys (TO BE DONE)
+})
