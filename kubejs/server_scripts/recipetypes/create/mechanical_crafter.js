9b081d97c957317aad1013cab925580a046521ab
AK9 <anthonkrisat@hotmail.com>
2024-03-15 08:30:13 +0100

    change angel ring recipe

--- kubejs/server_scripts/recipetypes/create/mechanical_crafter.js
+++ kubejs/server_scripts/recipetypes/create/mechanical_crafter.js
@@ -6,7 +6,7 @@ ServerEvents.recipes(e => {
     'modern_industrialization:electric_age/machine/assembler/replicator',
     'indrev:shaped/drill_bottom'
   ];
-  REMOVED_RECIPE.forEach(id => e.remove({id: id}));
+  REMOVED_RECIPE.forEach(id => e.remove({ id: id }));
 
   // Replicator
   e.custom({
@@ -34,217 +34,217 @@ ServerEvents.recipes(e => {
     }
   })
 
-// Angel Ring
-e.custom({
-"type": "create:mechanical_crafting",
-"acceptMirrored": false,
-"key": {
-  "U": {
-    "item": "kubejs:hyper_nitro_crystal"
-  },
-  "T": {
-    "item": 'indrev:module_feather_falling'
-  },
-  "C": {
-    "item": 'piercingpaxels:upgrade_unbreakable'
-  },
-  "B": {
-    "item": "techreborn:superconductor"
-  },
-  "S": {
-    "item": 'spectrum:take_off_belt'
-  },
-  "D": {
-    "item": 'spectrum:puff_circlet'
-  },
-  "G": {
-    "item": "kibe:water_ring"
-  },
-  "E": {
-    "item": "kibe:magma_ring"
-  },
-  "H": {
-    "item": "kibe:diamond_ring"
-  },
-  "O": {
-    "item": 'ad_astra:ostrum_engine'
-  },
-  "P": {
-    "item": 'botania:lens_flare'
-  },
-  "Q": {
-    "item": 'botania:flare_chakram'
-  },
-  "F": {
-    "item": 'create_jetpack:netherite_jetpack'
-  },
-  "L": {
-    "item": 'artifacts:cloud_in_a_bottle'
-  },
-  "J": {
-    "item": 'artifacts:helium_flamingo'
-  },
-  "M": {
-    "item": "kubejs:mother_of_all_pearls"
-  }
-},
-"pattern": [
-    'UQTBO',
-    'QDEJB',
-    'CGHGM',
-    'BLESP',
-    'OBFPU'
-],
-"result": {
-  "count": 1,
-  "item": "kibe:angel_ring"
-}
-}).id('aof:kibe/angel_ring')
-
-// Mother of All Pearls
-e.custom({
-"type": "create:mechanical_crafting",
-"acceptMirrored": false,
-"key": {
-    "A": {
-        "item": 'zenith:mythic_material'
-    },
-    "B": {
-        "item": 'botania:mana_pearl'
-    },
-    "C": {
-        "item": 'powah:aerial_pearl'
-    },
-    "D": {
-        "item": 'endermanoverhaul:icy_pearl'
-    },
-    "E": {
-        "item": 'ae2:fluix_pearl'
-    },
-    "F": {
-        "item": 'endermanoverhaul:corrupted_pearl'
-    },
-    "G": {
-        "item": 'bosses_of_mass_destruction:charged_ender_pearl'
-    },
-    "H": {
-        "item": 'endermanoverhaul:summoner_pearl'
-    },
-    "I": {
-        "item": 'endermanoverhaul:warped_pearl'
-    },
-    "J": {
-        "item": 'endermanoverhaul:soul_pearl'
-    },
-    "K": {
-        "item": 'endermanoverhaul:ancient_pearl'
-    },
-    "L": {
-        "item": 'endermanoverhaul:bubble_pearl'
-    },
-    "M": {
-        "item": 'endermanoverhaul:crimson_pearl'
-    }
-},
-"pattern": [
-    '  A  ',
-    ' BCD ',
-    'EFGHI',
-    ' JKL ',
-    '  M  '
-],
-"result": {
-    "count": 1,
-    "item": "kubejs:mother_of_all_pearls"
-}
-}).id('aof:materials/mother_of_all_pearls')
-
-// Mining Rig
-e.custom({
-  "type": "create:mechanical_crafting",
-  "acceptMirrored": false,
-  "key": {
-      "A": {
-          "item": 'indrev:machine_block'
+  // Angel Ring
+  e.custom({
+    "type": "create:mechanical_crafting",
+    "acceptMirrored": false,
+    "key": {
+      "U": {
+        "item": "kubejs:hyper_nitro_crystal"
       },
-      "B": {
-          "item": 'modern_industrialization:steel_large_plate'
+      "T": {
+        "item": 'indrev:module_feather_falling'
       },
       "C": {
-          "item": 'modern_industrialization:steel_gear'
+        "item": 'piercingpaxels:upgrade_unbreakable'
+      },
+      "B": {
+        "item": "techreborn:superconductor"
+      },
+      "S": {
+        "item": 'spectrum:take_off_belt'
       },
       "D": {
-          "item": 'powah:battery_niotic'
-      },
-      "E": {
-          "item": 'createaddition:electric_motor'
-      },
-      "F": {
-          "item": 'techreborn:industrial_machine_casing'
+        "item": 'spectrum:puff_circlet'
       },
       "G": {
-          "item": 'indrev:mining_drill_mk4'
+        "item": "kibe:water_ring"
+      },
+      "E": {
+        "item": "kibe:magma_ring"
       },
       "H": {
-          "item": 'techreborn:industrial_circuit'
+        "item": "kibe:diamond_ring"
       },
-      "I": {
-          "item": 'modern_industrialization:steel_barrel'
+      "O": {
+        "item": 'ad_astra:ostrum_engine'
+      },
+      "P": {
+        "item": 'botania:lens_flare'
+      },
+      "Q": {
+        "item": 'botania:flare_chakram'
+      },
+      "F": {
+        "item": 'besmirchment:final_broom'
+      },
+      "L": {
+        "item": 'artifacts:cloud_in_a_bottle'
       },
       "J": {
-          "item": 'computercraft:computer_normal',
+        "item": 'artifacts:helium_flamingo'
+      },
+      "M": {
+        "item": "kubejs:mother_of_all_pearls"
+      }
+    },
+    "pattern": [
+      'UQTBO',
+      'QDEJB',
+      'CGHGM',
+      'BLESP',
+      'OBFPU'
+    ],
+    "result": {
+      "count": 1,
+      "item": "kibe:angel_ring"
+    }
+  }).id('aof:kibe/angel_ring')
+
+  // Mother of All Pearls
+  e.custom({
+    "type": "create:mechanical_crafting",
+    "acceptMirrored": false,
+    "key": {
+      "A": {
+        "item": 'zenith:mythic_material'
+      },
+      "B": {
+        "item": 'botania:mana_pearl'
+      },
+      "C": {
+        "item": 'powah:aerial_pearl'
+      },
+      "D": {
+        "item": 'endermanoverhaul:icy_pearl'
+      },
+      "E": {
+        "item": 'ae2:fluix_pearl'
+      },
+      "F": {
+        "item": 'endermanoverhaul:corrupted_pearl'
+      },
+      "G": {
+        "item": 'bosses_of_mass_destruction:charged_ender_pearl'
+      },
+      "H": {
+        "item": 'endermanoverhaul:summoner_pearl'
+      },
+      "I": {
+        "item": 'endermanoverhaul:warped_pearl'
+      },
+      "J": {
+        "item": 'endermanoverhaul:soul_pearl'
+      },
+      "K": {
+        "item": 'endermanoverhaul:ancient_pearl'
+      },
+      "L": {
+        "item": 'endermanoverhaul:bubble_pearl'
+      },
+      "M": {
+        "item": 'endermanoverhaul:crimson_pearl'
+      }
+    },
+    "pattern": [
+      '  A  ',
+      ' BCD ',
+      'EFGHI',
+      ' JKL ',
+      '  M  '
+    ],
+    "result": {
+      "count": 1,
+      "item": "kubejs:mother_of_all_pearls"
+    }
+  }).id('aof:materials/mother_of_all_pearls')
+
+  // Mining Rig
+  e.custom({
+    "type": "create:mechanical_crafting",
+    "acceptMirrored": false,
+    "key": {
+      "A": {
+        "item": 'indrev:machine_block'
+      },
+      "B": {
+        "item": 'modern_industrialization:steel_large_plate'
+      },
+      "C": {
+        "item": 'modern_industrialization:steel_gear'
+      },
+      "D": {
+        "item": 'powah:battery_niotic'
+      },
+      "E": {
+        "item": 'createaddition:electric_motor'
+      },
+      "F": {
+        "item": 'techreborn:industrial_machine_casing'
+      },
+      "G": {
+        "item": 'indrev:mining_drill_mk4'
+      },
+      "H": {
+        "item": 'techreborn:industrial_circuit'
+      },
+      "I": {
+        "item": 'modern_industrialization:steel_barrel'
+      },
+      "J": {
+        "item": 'computercraft:computer_normal',
       },
       "K": {
         "item": 'modern_industrialization:steel_drill',
-    }
-  },
-  "pattern": [
+      }
+    },
+    "pattern": [
       ' BFB ',
       'BGHGB',
       'CEAEC',
       'BJDIB',
       ' BKB '
-  ],
-  "result": {
+    ],
+    "result": {
       "count": 1,
       "item": 'indrev:mining_rig_mk4'
-  }
+    }
   }).id('aof:machines/mining_rig')
 
   // Mining Rig Drill
-e.custom({
-  "type": "create:mechanical_crafting",
-  "acceptMirrored": false,
-  "key": {
+  e.custom({
+    "type": "create:mechanical_crafting",
+    "acceptMirrored": false,
+    "key": {
       "A": {
-          "item": 'indrev:machine_block'
+        "item": 'indrev:machine_block'
       },
       "B": {
-          "item": 'modern_industrialization:steel_large_plate'
+        "item": 'modern_industrialization:steel_large_plate'
       },
       "C": {
-          "item": 'computercraft:computer_normal'
+        "item": 'computercraft:computer_normal'
       },
       "D": {
-          "item": 'powah:battery_blazing'
+        "item": 'powah:battery_blazing'
       },
       "E": {
-          "item": 'modern_industrialization:steel_gear'
+        "item": 'modern_industrialization:steel_gear'
       },
       "F": {
-          "item": 'modern_industrialization:steel_drill'
+        "item": 'modern_industrialization:steel_drill'
       }
-  },
-  "pattern": [
+    },
+    "pattern": [
       ' BCB ',
       ' BDB ',
       ' BAB ',
       ' BEB ',
       ' BFB '
-  ],
-  "result": {
+    ],
+    "result": {
       "count": 1,
       "item": 'indrev:drill_bottom'
-  }
+    }
   }).id('aof:components/mining_rig_drill')
 })
