62fdd7545d0f4e095702d5dc8c2d9aba502ad920
AK9 <anthonkrisat@hotmail.com>
2024-03-18 02:16:22 +0100

    add remove/add coins script

--- kubejs/server_scripts/loot_tables/money_loot.js
+++ kubejs/server_scripts/loot_tables/money_loot.js
@@ -1,5 +1,28 @@
 LootJS.modifiers((event) => {
+
+  // Bosses
     event
       .addEntityLootModifier("bosses_of_mass_destruction:lich").addWeightedLoot([7,15],[Item.of("numismatic-overhaul:silver_coin").withChance(100)])
       .addLoot(Item.of("numismatic-overhaul:gold_coin").withChance(1));
 });
+
+ServerEvents.tags('entity_type', event => {
+
+  // This list ADDS coins to loottable.
+    const ADD_COINS = [
+      'minecraft:creeper'
+    ];
+  
+  // This list REMOVES coins from loottable.
+    const REMOVE_COINS = [
+      'minecraft:pillager'
+    ];
+  
+    ADD_COINS.forEach((entity) => {
+      event.add('numismatic-overhaul:the_bourgeoisie', entity)
+    });
+  
+    REMOVE_COINS.forEach((entity) => {
+      event.('numismatic-overhaul:the_bourgeoisie', entity)
+    });
+  });
