ac082a0eec86e3614475f8bb1395d8b9c1e853cd
Kazuhiko-Gushiken <just.a.stem.nerd@gmail.com>
2024-03-17 18:23:27 -0700

    adding more bosses to drop money

--- kubejs/server_scripts/loot_tables/money_loot.js
+++ kubejs/server_scripts/loot_tables/money_loot.js
@@ -2,8 +2,14 @@ LootJS.modifiers((event) => {
 
   // Bosses
   event
-    .addEntityLootModifier("bosses_of_mass_destruction:lich").addWeightedLoot([7, 15], [Item.of("numismatic-overhaul:silver_coin").withChance(100)])
+    .addEntityLootModifier("bosses_of_mass_destruction:lich").addWeightedLoot([7,15],[Item.of("numismatic-overhaul:silver_coin").withChance(100)])
     .addLoot(Item.of("numismatic-overhaul:gold_coin").withChance(1));
+  event
+    .addEntityLootModifier("minecraft:ender_dragon").addWeightedLoot([4,10],[Item.of("numismatic-overhaul:silver_coin").withChance(100)]);
+  event
+    .addEntityLootModifier("minecraft:warden").addWeightedLoot([3,5],[Item.of("numismatic-overhaul:silver_coin").withChance(50)]);
+  event
+    .addEntityLootModifier("minecells:conjunctivius").addWeightedLoot([5,13],[Item.of("numismatic-overhaul:silver_coin").withChance(100)]);
 });
 
 ServerEvents.tags('entity_type', event => {
